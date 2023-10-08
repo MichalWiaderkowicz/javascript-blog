2968bd5a6565c1d6807fdfa1da73737358cfecd2
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2023-10-09 00:09:03 +0200

    add all tasks 5.4 bug fix

--- js/script.js
+++ js/script.js
@@ -31,9 +31,44 @@ const targetArticle = document.querySelector(articleSelector);
   /* [DONE] add class 'active' to the correct article */
 targetArticle.classList.add('active');  
 }
-  
-  const links = document.querySelectorAll('.titles a');
-  
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+  /* find all the articles and save them to variable: articles */
+  let articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+    for(let article of articles){  
+  /* for each article */
+  //const articles = document.querySelectorAll(optArticleSelector);
+   //for (const article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+    /* [DONE innerHML;] get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /*[!!!zapytaj o insertAdjacentHTML!!!] insert link into titleList */
+    //titleList.innerHTML = titleList.innerHTML + linkHTML;
+    /* insert link into html variable */
+    html = html + linkHTML;
+   }
+   titleList.innerHTML = html;
+   const links = document.querySelectorAll('.titles a');
+  console.log(links)
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
+}
+}
+
+generateTitleLinks();
